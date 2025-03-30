f623022a5828e3adb218d234fe072007bc5ff524
yukito <iyggf66974@yahoo.ne.jp>
2025-03-30 19:49:52 +0900

    migrate ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_30_083657) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_30_104849) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -69,6 +69,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_30_083657) do
     t.string "industry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_icon"
+    t.string "bg_image"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
