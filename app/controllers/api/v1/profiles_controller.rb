edd0185382b85414e6b3d8783b294e5d6a08aafc
yukito <iyggf66974@yahoo.ne.jp>
2025-03-31 13:35:01 +0900

    profilePage img追加　成功

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -4,14 +4,25 @@ class Api::V1::ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :update]
 
   def show
-    render json: @profile.as_json(include: [:user_icon, :bg_image])
+    user_icon_url = @profile.user_icon.attached? ? url_for(@profile.user_icon) : nil
+    bg_image_url = @profile.bg_image.attached? ? url_for(@profile.bg_image) : nil
+
+    render json: @profile.as_json.merge(user_icon_url: user_icon_url, bg_image_url: bg_image_url)
   end
 
   def create
     @profile = current_user.build_profile(profile_params)
     if @profile.save
-      attach_images
-      render json: @profile.as_json(include: [:user_icon, :bg_image]), status: :created
+      if params[:user_icon]
+        @profile.user_icon.attach(params[:user_icon])
+      end
+      if params[:bg_image]
+        @profile.bg_image.attach(params[:bg_image])
+      end
+      user_icon_url = @profile.user_icon.attached? ? url_for(@profile.user_icon) : nil
+      bg_image_url = @profile.bg_image.attached? ? url_for(@profile.bg_image) : nil
+
+      render json: @profile.as_json.merge(user_icon_url: user_icon_url, bg_image_url: bg_image_url), status: :created
     else
       render json: { errors: @profile.errors.full_messages }, status: :unprocessable_entity
     end
@@ -19,8 +30,16 @@ class Api::V1::ProfilesController < ApplicationController
 
   def update
     if @profile.update(profile_params)
-      attach_images
-      render json: @profile.as_json(include: [:user_icon, :bg_image])
+      if params[:user_icon]
+        @profile.user_icon.attach(params[:user_icon])
+      end
+      if params[:bg_image]
+        @profile.bg_image.attach(params[:bg_image])
+      end
+      user_icon_url = @profile.user_icon.attached? ? url_for(@profile.user_icon) : nil
+      bg_image_url = @profile.bg_image.attached? ? url_for(@profile.bg_image) : nil
+
+      render json: @profile.as_json.merge(user_icon_url: user_icon_url, bg_image_url: bg_image_url)
     else
       render json: { errors: @profile.errors.full_messages }, status: :unprocessable_entity
     end
@@ -34,14 +53,5 @@ class Api::V1::ProfilesController < ApplicationController
 
   def set_profile
     @profile = current_user.profile
-  end
-
-  def attach_images
-    if params[:user_icon]
-      @profile.user_icon.attach(params[:user_icon])
-    end
-    if params[:bg_image]
-      @profile.bg_image.attach(params[:bg_image])
-    end
   end
 end
